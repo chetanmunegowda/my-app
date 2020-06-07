26e40942151821507b8bb1e8ba8d71c8f8c050b7
Chetan Munegowda <chetan.munegowda@semanticbits.com>
2020-06-07 13:05:20 -0400

    Updates

---
+++ precache-manifest.4af72df535b02b56ba5d0dbe30d5c83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f3affc1a305fee216665e30fbb50ad",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6591aa925c88c1be4a7b",
+    "url": "/my-app/static/css/main.81ca9a81.chunk.css"
+  },
+  {
+    "revision": "00fcca3ec42227361113",
+    "url": "/my-app/static/js/2.465df07d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.465df07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6591aa925c88c1be4a7b",
+    "url": "/my-app/static/js/main.0c0d124a.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
