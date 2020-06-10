01681ce7456c46a34c8f5370a30c8ca42db881af
Chetan Munegowda <chetan.munegowda@semanticbits.com>
2020-06-09 22:14:55 -0400

    adjust css

--- src/component/Summary.js
+++ src/component/Summary.js
@@ -4,11 +4,9 @@ import './Summary.css'
 export default class Summary extends React.Component {
   render () {
     return (
-      <>
-        <section>
-          <p>Energetic software engineer with 6.5+ years of experience developing robust code for high-volume businesses. Increased code-efficiency, decreased rework and costs and thereby boosted customer retention appreciably. Passionate about building world-class web applications, an enthusiastic team player and deep creative thinker</p>
-        </section>
-      </>
+      <div className="summary-container">
+        <p>Energetic software engineer with 6.5+ years of experience developing robust code for high-volume businesses. Increased code-efficiency, decreased rework and costs and thereby boosted customer retention appreciably. Passionate about building world-class web applications, an enthusiastic team player and deep creative thinker</p>
+      </div>
     );
   }
 }
